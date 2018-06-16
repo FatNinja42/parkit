0d2740e9a32fc672ca565e37095c88eacbf31b74
Marton Cserei <Marton.Cserei@iquestgroup.com>
2018-06-16 15:49:35 +0300

    implemented list component

--- parkit-front/src/app/parking-table/parking-table.component.ts
+++ parkit-front/src/app/parking-table/parking-table.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatSort } from '@angular/material';
 import { ParkingTableDataSource } from './parking-table-datasource';
+import { UserListElement } from '../models/user.model';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'parking-table',
   templateUrl: './parking-table.component.html',
-  styleUrls: ['./parking-table.component.css']
+  styleUrls: ['./parking-table.component.scss']
 })
 export class ParkingTableComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   dataSource: ParkingTableDataSource;
 
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ['index', 'name', 'parkingSpot'];
+
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
     this.dataSource = new ParkingTableDataSource(this.paginator, this.sort);
+    this.authService.logIn();
+  }
+
+  hasNoSpot(user: UserListElement) {
+    return user.parkingSpot === 0;
+  }
+
+  isCurrentUser(row: UserListElement) {
+    return row.id === this.authService.getUser().id;
   }
 }
